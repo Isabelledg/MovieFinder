1a9661954fc9827ae4f31923fd16035fddd19e79
isabelle de geer <isabelledegeer1@gmail.com>
2021-11-29 17:50:47 +0000

    added the def to the group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,17 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def index
+    @group = Group.all
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def destroy
+    @group = Group.delete(params[:id])
+  end
 end

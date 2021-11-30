428ff346990f58b38d5e86e9090c4480849eef82
Raul Gavris <ra.gavris@gmail.com>
2021-11-30 19:04:41 +0000

    groups created

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,8 +3,12 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   def index
-    @group = Group.all
+    @groups = Group.all
   end
 
   def edit
@@ -13,5 +17,25 @@ class GroupsController < ApplicationController
 
   def destroy
     @group = Group.delete(params[:id])
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    @group.users << current_user
+    if @group.save!
+      redirect_to @group
+    else
+      redirect_to @group
+    end
+  end
+
+  def create
+    @group = Group.new(params[:name])
+    @group.users << current_user
+    if @group.save!
+      redirect_to @group
+    else
+      render :new
+    end
   end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  has_many :user_groups
+  has_many :users, through: :user_groups
 end
